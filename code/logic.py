8f2cff14fdb662c408e456b9f5592e31e36b5911
LordDomon <lorddomon001@gmail.com>
2025-04-20 21:30:01 +0200

    Register user added

--- code/logic.py
+++ code/logic.py
@@ -2,6 +2,7 @@ from telebot import TeleBot, types
 import sqlite3
 from config import *
 import os
+import time
 
 bot = TeleBot(token)
 
@@ -20,9 +21,34 @@ class DB_Manager:
                              """)
     
     def start(self, message):
-        bot.send_message(message.chat.id, "hello")
-        video_path = "C:\\Users\\Admin\\OneDrive\\Desktop\\simulator\\images\\vstuplenie.mp4"
-        if os.path.exists(video_path):
-         with open(video_path, "rb") as f:
-           bot.send_video(message.chat.id, f)
+        user_id = message.chat.id
+        username = message.from_user.username or "Anonymous"
+        with self.conn:
+           cur = self.conn.cursor()
+           cur.execute("SELECT * FROM USERS WHERE user_id = ?", (user_id,))
+           user = cur.fetchone()
+           
+           if user is None:
+              balance = 0
+              cur.execute('''INSERT INTO users (user_id, username, balance) VALUES (?, ?, ?)''', (user_id, username, balance))
+              bot.send_message(user_id, "Данный проект был создан Дмитрием Горским и Родионом Кундянок")
+              bot.send_message(user_id, "Вступление начинается, подождите немного.")
+              bot.send_chat_action(user_id, "upload_video") 
+        
+              video_path = "C:\\Users\\Admin\\OneDrive\\Desktop\\simulator\\images\\vstuplenie.mp4"
+              if os.path.exists(video_path):
+               with open(video_path, "rb") as f:
+                bot.send_video(user_id, f)
+
+              time.sleep(2)
+            
+           elif user is not None:
+              pass
+       
+
+             
+
+              
+
+                
 
--- code/main.py
+++ code/main.py
@@ -10,6 +10,10 @@ manager = DB_Manager(database)
 @bot.message_handler(commands=['start'])
 def start_command(message):
     manager.start(message)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add("Профиль", "Работа")
+    bot.send_message(message.chat.id, f"""Выберете действие:
+    """, reply_markup=markup)
 
 if __name__ == "__main__":
     manager.create_tables()
