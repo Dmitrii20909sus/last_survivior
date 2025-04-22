50aca672f966389c3db383186a99e1d005ab07bb
LordDomon <lorddomon001@gmail.com>
2025-04-22 14:22:01 +0200

    lin gan guli guli guli wacha lin gan guuu lin gan guuuu

--- code/logic.py
+++ code/logic.py
@@ -3,6 +3,7 @@ import sqlite3
 from config import *
 import os
 import time
+from datetime import datetime, timedelta
 
 bot = TeleBot(token)
 
@@ -17,9 +18,19 @@ class DB_Manager:
                              id INTEGER PRIMARY KEY,
                              username TEXT,
                              user_id TEXT UNIQUE,
-                             balance REAL DEFAULT 0)
+                             gold INTEGER DEFAULT 0,
+                             wood INTEGER DEFAULT 0,
+                             stone INTEGER DEFAULT 0,
+                             story INTEGER DEFAULT 0)
                              """)
-    
+    def select_user(self, message):
+       user_id = message.chat.id
+       with self.conn:
+        cur = self.conn.cursor()
+        cur.execute("SELECT * FROM USERS WHERE user_id = ?", (user_id,))
+        user = cur.fetchone()
+       return user
+
     def start(self, message):
         user_id = message.chat.id
         username = message.from_user.username or "Anonymous"
@@ -29,21 +40,68 @@ class DB_Manager:
            user = cur.fetchone()
            
            if user is None:
-              balance = 0
-              cur.execute('''INSERT INTO users (user_id, username, balance) VALUES (?, ?, ?)''', (user_id, username, balance))
+              gold = 0
+              wood = 0
+              stone = 0
+              story = 0
+              cur.execute('''INSERT INTO users (user_id, username, gold, wood, stone, story) VALUES (?, ?, ?, ?, ?, ?)''', (user_id, username, gold, wood, stone, story))
               bot.send_message(user_id, "Данный проект был создан Дмитрием Горским и Родионом Кундянок")
-              bot.send_message(user_id, "Вступление начинается, подождите немного.")
-              bot.send_chat_action(user_id, "upload_video") 
-        
               video_path = "C:\\Users\\Admin\\OneDrive\\Desktop\\simulator\\images\\vstuplenie.mp4"
+              bot.send_message(user_id, "Вступление загружается, подождите немного.")
+              bot.send_chat_action(user_id, "upload_video")
+            
+              time.sleep(1.5)
+    
               if os.path.exists(video_path):
                with open(video_path, "rb") as f:
-                bot.send_video(user_id, f)
-
+                bot.send_video(user_id, f, timeout=120)
+              else:
+               bot.send_message(user_id, "Видео не найдено. Короче без вступления :(") 
+        
               time.sleep(2)
             
            elif user is not None:
               pass
+
+    def story_0(self, message):
+       
+       user_id = message.chat.id
+       user = self.select_user(message)
+       bot.send_message(user_id, """*Бог:* Здравствуй, сын мой божий. 
+                        Ты один из единственных, кому удалось пережить
+                        это несчастье.""", parse_mode="Markdown")
+       time.sleep(2.5)
+       bot.send_message(user_id,"""*Бог:* Ну что-же, для начала награжу ка 
+                        я тебя как избранного ресурсами, в которых ты сейчас
+                        очень нуждаешься, держи 10 кусочков золота🏅, 20 кусков 
+                        дерева🪵 и 20 камней🪨, с ними ты сможешь построй 
+                        свой первый дом""", parse_mode="Markdown") 
+       time.sleep(2)
+       with self.conn:
+          cur = self.conn.cursor()
+          cur.execute("""
+            UPDATE users SET gold = gold + 10, wood = wood + 20, stone = stone + 20, story = 1 
+            WHERE user_id = ?
+        """, (user_id,))    
+       bot.send_message(user_id, "<i>Вам зачисленно:</i> +20🏅, +20🪵, +20🪨", parse_mode="HTML")  
+      
+       bot.send_message(user_id, """*Бог:* Скоро ты построишь себе новый дом,
+                        но сначала, иди ка ты на охоту, 
+                        ты очень голодный, тебе стоило бы поесть, 
+                        поэтому нажми в меню на кнопку *Охота*, 
+                        чтобы добыть еды""", parse_mode="Markdown")
+       # Ich soll den Markdown Ohota erstellen und dort  2 bis 5 Tiere jagen,
+       # jedes Tier gibt mir bestimmte Anzahl an Punkten die dann alle 
+       # zusammen summiert werden ---> noch eine Tabelle erstellen (Zweri)
+       #Er soll ungefähr so aussehen:
+       #Ihre Beute:
+       # () Krolikov
+       # () Volkov
+       # () Kasul
+       # ...
+       # Ich soll außerdem ein cooldown für die Jagd erstellen
+       # Ich muss den Gottes Text an die richtigen Zeilen setzen
+       # Profil soll auch angezeigt werden
        
 
              
--- code/main.py
+++ code/main.py
@@ -9,11 +9,32 @@ manager = DB_Manager(database)
 
 @bot.message_handler(commands=['start'])
 def start_command(message):
+    user_id = message.chat.id
     manager.start(message)
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add("Профиль", "Работа")
-    bot.send_message(message.chat.id, f"""Выберете действие:
-    """, reply_markup=markup)
+
+    user = manager.select_user(message)
+
+    if user:
+        story = user[6]
+        if story == 0:
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            markup.add("Профиль", "Начать сюжет")
+            bot.send_message(user_id, f"""Выберете действие:
+            """, reply_markup=markup)
+
+@bot.message_handler(func=lambda message: message.text == "Начать сюжет")
+def work_keyboard(message):
+    user = manager.select_user(message)
+    user_id = message.chat.id
+
+    if user:
+        story = user[6]
+        if story != 0:
+            bot.send_message(user_id, f"""Вы уже прошли начало сюжета  """, )
+        elif story == 0:
+          manager.story_0(message)
+
+    
 
 if __name__ == "__main__":
     manager.create_tables()
